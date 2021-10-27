bb057a56aff1f34ecffb86a3a81e3e0437224a67
Zechariah <zechariahtan144@gmail.com>
2021-10-28 00:52:07 +0800

    Refresh access token every time the process starts

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,131 @@
 import AfterEvery from "after-every"
 import { Client, Intents } from "discord.js"
+import express from "express"
+import open from "open"
+import path from "path"
+import qs from "qs"
+import fs from "fs/promises"
 import GuildCache from "./models/GuildCache"
 import BotSetupHelper from "./utilities/BotSetupHelper"
+import axios from "axios"
 
 const config = require("../config.json")
 
-// region Initialize bot
-const bot = new Client({
-	intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]
+const PORT = 4296
+const app = express()
+
+app.get("/", async (req, res) => {
+	console.log(`Got Spotify API Authorization token`)
+	const code = req.query.code as string
+
+	// Get spotify access token from authorization code
+	const spotify_res = await axios.post(
+		"https://accounts.spotify.com/api/token",
+		qs.stringify({
+			grant_type: "authorization_code",
+			code,
+			redirect_uri: "http://localhost:4296"
+		}),
+		{
+			headers: {
+				Authorization:
+					"Basic " +
+					Buffer.from(
+						config.spotify.clientId + ":" + config.spotify.clientSecret
+					).toString("base64"),
+				"Content-Type": "application/x-www-form-urlencoded"
+			}
+		}
+	)
+
+	// Replace old access token with new access token
+	console.log(`Got Spotify API Access token`)
+	const config_path = path.join(__dirname, "../config.json")
+	const config_data = await fs.readFile(config_path, "utf8")
+	await fs.writeFile(
+		config_path,
+		config_data
+			.replace(config.spotify.accessToken, spotify_res.data.access_token)
+			.replace(config.spotify.refreshToken, spotify_res.data.refresh_token)
+	)
+	console.log(`Replaced Spotify API Access token`)
+
+	res.send("<script>window.close();</script>")
+	server.close()
+
+	// Start the bot
+	start_bot()
 })
-const botSetupHelper = new BotSetupHelper(bot)
-const { cache: botCache } = botSetupHelper
-// endregion
 
-void bot.login(config.discord.token)
-bot.on("ready", async () => {
-	console.log("Logged in as SounDroid Bot#5566")
+const server = app.listen(PORT, async () => {
+	console.log(`Server running on port ${PORT}`)
 
-	let debugCount = 0
+	// Get spotify authorization code
+	const spotify_url = new URL("https://accounts.spotify.com/authorize")
+	spotify_url.searchParams.append("response_type", "code")
+	spotify_url.searchParams.append("client_id", config.spotify.clientId)
+	spotify_url.searchParams.append("redirect_uri", "http://localhost:4296")
+	spotify_url.searchParams.append("scope", "user-read-private playlist-read-private")
+	await open(spotify_url.href, { wait: true })
+})
 
-	let i = 0
-	let count = bot.guilds.cache.size
-	for (const guild of bot.guilds.cache.toJSON()) {
-		const tag = `${(++i).toString().padStart(count.toString().length, "0")}/${count}`
-		let cache: GuildCache | undefined
-		try {
-			cache = await botCache.getGuildCache(guild)
-		} catch (err) {
-			console.error(`${tag} ❌ Couldn't find a Firebase Document for Guild(${guild.name})`)
-			guild.leave()
-			continue
-		}
-
-		try {
-			await botSetupHelper.deploySlashCommands(guild)
-		} catch (err) {
-			console.error(
-				`${tag} ❌ Couldn't get Slash Command permission for Guild(${guild.name})`
-			)
-			guild.leave()
-			continue
-		}
-
-		cache.updateMinutely(debugCount)
-
-		console.log(`${tag} ✅ Restored cache for Guild(${guild.name})`)
-	}
-	console.log(`✅ All bot cache restored`)
-	console.log("|")
-
-	AfterEvery(1).minutes(async () => {
-		debugCount++
-		for (const guild of bot.guilds.cache.toJSON()) {
-			const cache = await botCache.getGuildCache(guild)
-			cache.updateMinutely(debugCount)
-		}
+const start_bot = () => {
+	// region Initialize bot
+	const bot = new Client({
+		intents: [
+			Intents.FLAGS.GUILD_VOICE_STATES,
+			Intents.FLAGS.GUILD_MESSAGES,
+			Intents.FLAGS.GUILDS
+		]
 	})
-})
+	const botSetupHelper = new BotSetupHelper(bot)
+	const { cache: botCache } = botSetupHelper
+	// endregion
+
+	void bot.login(config.discord.token)
+	bot.on("ready", async () => {
+		console.log("Logged in as SounDroid Bot#5566")
+
+		let debugCount = 0
+
+		let i = 0
+		let count = bot.guilds.cache.size
+		for (const guild of bot.guilds.cache.toJSON()) {
+			const tag = `${(++i).toString().padStart(count.toString().length, "0")}/${count}`
+			let cache: GuildCache | undefined
+			try {
+				cache = await botCache.getGuildCache(guild)
+			} catch (err) {
+				console.error(
+					`${tag} ❌ Couldn't find a Firebase Document for Guild(${guild.name})`
+				)
+				guild.leave()
+				continue
+			}
+
+			try {
+				await botSetupHelper.deploySlashCommands(guild)
+			} catch (err) {
+				console.error(
+					`${tag} ❌ Couldn't get Slash Command permission for Guild(${guild.name})`
+				)
+				guild.leave()
+				continue
+			}
+
+			cache.updateMinutely(debugCount)
+
+			console.log(`${tag} ✅ Restored cache for Guild(${guild.name})`)
+		}
+		console.log(`✅ All bot cache restored`)
+		console.log("|")
+
+		AfterEvery(1).minutes(async () => {
+			debugCount++
+			for (const guild of bot.guilds.cache.toJSON()) {
+				const cache = await botCache.getGuildCache(guild)
+				cache.updateMinutely(debugCount)
+			}
+		})
+	})
+}
--- src/utilities/ApiHelper.ts
+++ src/utilities/ApiHelper.ts
@@ -100,8 +100,9 @@ export default class ApiHelper {
 	}
 
 	public async findSpotifySong(trackId: string, requester: string): Promise<Song> {
-		const accessToken = (await this.spotifyApi.refreshAccessToken()).body.access_token
-		this.spotifyApi.setAccessToken(accessToken)
+		const refresh_response = (await this.spotifyApi.refreshAccessToken()).body
+		this.spotifyApi.setAccessToken(refresh_response.access_token)
+		this.spotifyApi.setRefreshToken(refresh_response.refresh_token || config.spotify.refreshToken)
 
 		const result = (await this.spotifyApi.getTrack(trackId)).body
 		return new Song(
