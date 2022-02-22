ae726afbadc60588e0b6062f72b40bfe96305fff
Zechariah <zechariahtan144@gmail.com>
2022-02-22 14:44:15 +0800

    Show a "Skeleton" before Genius API responds
    I get an Unknown Interaction if I don't defer an interaction after a short period of time

--- src/interactions/selectMenus/lyrics-query.ts
+++ src/interactions/selectMenus/lyrics-query.ts
@@ -3,6 +3,7 @@ import Entry from "../../data/Entry"
 import GuildCache from "../../data/GuildCache"
 import { Emoji, iSelectMenuFile, ResponseBuilder } from "nova-bot"
 import { GuildMember, Message, MessageEmbed } from "discord.js"
+import { useTryAsync } from "no-try"
 
 const file: iSelectMenuFile<Entry, GuildCache> = {
 	defer: false,
@@ -10,42 +11,56 @@ const file: iSelectMenuFile<Entry, GuildCache> = {
 	execute: async helper => {
 		const member = helper.interaction.member as GuildMember
 
-		try {
-			const { title, artiste, cover, lyrics } = await helper.cache.apiHelper.findGeniusLyrics(
-				helper.value()!
-			)
-			helper.interaction.update({
-				embeds: [
-					new MessageEmbed()
-						.setTitle(`Genius Lyrics for: ${title} - ${artiste}`)
-						.setColor(await new DominantColorGetter(cover).getColor())
-						.setThumbnail(cover)
-						.setDescription(lyrics)
-						.setFooter({
-							text: `Requested by @${member.displayName}`,
-							iconURL: member.user.displayAvatarURL()
-						})
-				],
-				components: []
-			})
-		} catch (err) {
-			logger.error(err)
-			helper.interaction.update({
-				embeds: [
-					new ResponseBuilder(
-						Emoji.BAD,
-						"Failed to retrieve lyrics from Genius API"
-					).build()
-				],
-				components: []
-			})
-		}
+		await helper.interaction.update({
+			embeds: [
+				new MessageEmbed()
+					.setTitle("Genius Lyrics for: `<...>` - `<...>`")
+					.setDescription("`<...>`")
+					.setFooter({
+						text: `Requested by @${member.displayName}`,
+						iconURL: member.user.displayAvatarURL()
+					})
+			],
+			components: []
+		})
 
-		if (helper.interaction.message.type === "DEFAULT") {
-			setTimeout(() => {
-				const message = helper.interaction.message as Message
-				message.delete().catch(err => logger.warn("Failed to delete message", err))
-			}, 5000)
+		const [, messageOptions] = await useTryAsync(async () => {
+			try {
+				const { title, artiste, cover, lyrics } =
+					await helper.cache.apiHelper.findGeniusLyrics(helper.value()!)
+				return {
+					embeds: [
+						new MessageEmbed()
+							.setTitle(`Genius Lyrics for: ${title} - ${artiste}`)
+							.setColor(await new DominantColorGetter(cover).getColor())
+							.setThumbnail(cover)
+							.setDescription(lyrics)
+							.setFooter({
+								text: `Requested by @${member.displayName}`,
+								iconURL: member.user.displayAvatarURL()
+							})
+					],
+					components: []
+				}
+			} catch (err) {
+				logger.error(err)
+				return {
+					embeds: [
+						new ResponseBuilder(
+							Emoji.BAD,
+							"Failed to retrieve lyrics from Genius API"
+						).build()
+					],
+					components: []
+				}
+			}
+		})
+
+		if (helper.interaction.message.type === "APPLICATION_COMMAND") {
+			await helper.interaction.editReply(messageOptions)
+		} else {
+			const message = helper.interaction.message as Message
+			await message.edit(messageOptions)
 		}
 	}
 }
