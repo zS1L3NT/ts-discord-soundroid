3a1ae78c2b860d2bc11a9f0568f2658b2c75e791
Zechariah <zechariahtan144@gmail.com>
2022-03-05 23:50:58 +0800

    Fixed typescript errors

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,23 +1,19 @@
 declare module NodeJS {
-	interface Process {
-		env: {
-			readonly FIREBASE__SERVICE_ACCOUNT__PROJECT_ID: string,
-			readonly FIREBASE__SERVICE_ACCOUNT__PRIVATE_KEY: string,
-			readonly FIREBASE__SERVICE_ACCOUNT__CLIENT_EMAIL: string,
-			readonly FIREBASE__COLLECTION: string,
-			readonly FIREBASE__DATABASE_URL: string,
+	interface ProcessEnv {
+		readonly FIREBASE__SERVICE_ACCOUNT__PROJECT_ID: string
+		readonly FIREBASE__SERVICE_ACCOUNT__PRIVATE_KEY: string
+		readonly FIREBASE__SERVICE_ACCOUNT__CLIENT_EMAIL: string
+		readonly FIREBASE__COLLECTION: string
+		readonly FIREBASE__DATABASE_URL: string
 
-			readonly DISCORD__TOKEN: string,
-			readonly DISCORD__BOT_ID: string,
-			readonly DISCORD__DEV_ID: string,
+		readonly DISCORD__TOKEN: string
+		readonly DISCORD__BOT_ID: string
+		readonly DISCORD__DEV_ID: string
 
-			readonly SPOTIFY__REFRESH_TOKEN: string,
-			readonly SPOTIFY__CLIENT_ID: string,
-			readonly SPOTIFY__CLIENT_SECRET: string,
+		readonly SPOTIFY__REFRESH_TOKEN: string
+		readonly SPOTIFY__CLIENT_ID: string
+		readonly SPOTIFY__CLIENT_SECRET: string
 
-			readonly GENIUS__ACCESS_TOKEN: string
-
-			readonly LOG_LEVEL: string | undefined
-		}
+		readonly GENIUS__ACCESS_TOKEN: string
 	}
 }
