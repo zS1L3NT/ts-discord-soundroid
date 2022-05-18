bb99cc25a538b1560d74976dc11e3f438f374d8f
Zechariah <zechariahtan144@gmail.com>
2022-05-19 03:00:47 +0800

    🚚 move logger to it's own file

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,12 @@
 import "dotenv/config"
 
-import colors from "colors"
 import { Intents } from "discord.js"
 import http from "http"
 import NovaBot from "nova-bot"
 import path from "path"
-import Tracer from "tracer"
 
 import BotCache from "./data/BotCache"
 import GuildCache from "./data/GuildCache"
-
-/**
- * log: Used for basic primitive information
- * debug: Only used for debugging if not don't use this
- * info: Used for general information
- * alert: Used for possible minor problems or user erros
- * warn: Used for caught errors which need attention
- * error: Used for errors that are not fixed
- */
-global.logger = Tracer.colorConsole({
-	level: process.env.LOG_LEVEL || "log",
-	format: [
-		"[{{timestamp}}] <{{path}}> {{message}}",
-		{
-			//@ts-ignore
-			alert: "[{{timestamp}}] <{{path}}, Line {{line}}> {{message}}",
-			warn: "[{{timestamp}}] <{{path}}, Line {{line}}> {{message}}",
-			error: "[{{timestamp}}] <{{path}}, Line {{line}} at {{pos}}> {{message}}"
-		}
-	],
-	methods: ["log", "discord", "debug", "info", "alert", "warn", "error"],
-	dateformat: "dd mmm yyyy, hh:MM:sstt",
-	filters: {
-		log: colors.grey,
-		//@ts-ignore
-		discord: colors.cyan,
-		debug: colors.blue,
-		info: colors.green,
-		//@ts-ignore
-		alert: colors.yellow,
-		warn: colors.yellow.bold.italic,
-		error: colors.red.bold.italic
-	},
-	preprocess: data => {
-		data.path = data.path
-			.split("nova-bot")
-			.at(-1)!
-			.replace(/^(\/|\\)dist/, "nova-bot")
-			.replaceAll("\\", "/")
-		data.path = data.path
-			.split("ts-discord-soundroid")
-			.at(-1)!
-			.replace(/^(\/|\\)(dist|src)/, "src")
-			.replaceAll("\\", "/")
-	}
-})
 
 process.on("uncaughtException", err => {
 	if (err.message !== "The user aborted a request.") {
