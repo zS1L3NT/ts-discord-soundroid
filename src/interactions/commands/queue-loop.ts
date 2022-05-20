28871316ab3af8b020346a29edd7ad09974c93a4
Zechariah <zechariahtan144@gmail.com>
2022-05-20 15:03:14 +0800

    ✨ add queue-loop command

---
+++ src/interactions/commands/queue-loop.ts
@@ -0,0 +1,35 @@
+import { BaseCommand, CommandHelper, ResponseBuilder } from "nova-bot"
+
+import Entry from "../../data/Entry"
+import GuildCache from "../../data/GuildCache"
+import HasMusicServiceMiddleware from "../../middleware/HasMusicServiceMiddleware"
+import IsInVoiceChannelMiddleware from "../../middleware/IsInVoiceChannelMiddleware"
+
+export default class extends BaseCommand<Entry, GuildCache> {
+	override defer = true
+	override ephemeral = true
+	override data = {
+		name: "queue-loop",
+		description: "Loop the current queue, disables loop mode"
+	}
+
+	override middleware = [new IsInVoiceChannelMiddleware(), new HasMusicServiceMiddleware()]
+
+	override condition(helper: CommandHelper<Entry, GuildCache>) {
+		return helper.isMessageCommand(helper.cache.getPrefix(), "queue-loop", "only")
+	}
+
+	override converter(helper: CommandHelper<Entry, GuildCache>) {}
+
+	override async execute(helper: CommandHelper<Entry, GuildCache>) {
+		const service = helper.cache.service!
+
+		service.loop = false
+		service.queueLoop = !service.queueLoop
+
+		helper.respond(
+			ResponseBuilder.good(`Queue Loop ${service.queueLoop ? "enabled" : "disabled"}`)
+		)
+		helper.cache.updateMusicChannel()
+	}
+}
