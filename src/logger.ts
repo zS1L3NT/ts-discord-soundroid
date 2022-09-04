9bcadb8f5f70a1ccd7df8803d5d1057688711561
Zechariah <zechariahtan144@gmail.com>
2022-09-05 02:16:58 +0800

    ⬆️ update nova-bot to v3.0.0

--- src/logger.ts
+++ src/logger.ts
@@ -14,7 +14,6 @@ export default Tracer.colorConsole({
 	format: [
 		"[{{timestamp}}] <{{path}}> {{message}}",
 		{
-			//@ts-ignore
 			alert: "[{{timestamp}}] <{{path}}, Line {{line}}> {{message}}",
 			warn: "[{{timestamp}}] <{{path}}, Line {{line}}> {{message}}",
 			error: "[{{timestamp}}] <{{path}}, Line {{line}} at {{pos}}> {{message}}"
@@ -24,11 +23,9 @@ export default Tracer.colorConsole({
 	dateformat: "dd mmm yyyy, hh:MM:sstt",
 	filters: {
 		log: colors.grey,
-		//@ts-ignore
 		discord: colors.cyan,
 		debug: colors.blue,
 		info: colors.green,
-		//@ts-ignore
 		alert: colors.yellow,
 		warn: colors.yellow.bold.italic,
 		error: colors.red.bold.italic
